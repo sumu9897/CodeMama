166e907d5b5290d834ade721b06b316e10dbd550
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2023-11-17 11:42:48 +0600

    solve by python

---
+++ Quotient Calculator.py
@@ -0,0 +1,8 @@
+# Input two numbers
+numerator, denominator = map(int, input().split())
+
+# Calculate the quotient using integer division
+quotient = numerator // denominator
+
+# Output the quotient
+print("{}".format(quotient))
---
+++ Remainder Reminder.py
@@ -0,0 +1,8 @@
+# Input two numbers
+num1, num2 = map(int, input().split())
+
+# Calculate the remainder
+remainder = num1 % num2
+
+# Output the remainder
+print("{}".format(remainder))
