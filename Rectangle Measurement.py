e89cba7815fe80f1b3ecf028613c6fec240dde0a
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2023-11-17 11:40:12 +0600

    solve by python

---
+++ Rectangle Measurement.py
@@ -0,0 +1,8 @@
+# Input length and width of the rectangle
+length, width = map(int, input().split())
+
+# Calculate the area of the rectangle
+area = length * width
+
+# Output the area
+print("{}".format(area))
