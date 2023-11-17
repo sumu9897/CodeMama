fb39be3e54a0bc839ede9a1d7d930c023ecf2248
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2023-11-17 11:52:25 +0600

    solve by python

---
+++ Even or Odd.py
@@ -0,0 +1,8 @@
+# Input a number
+number = int(input())
+
+# Check if the number is even or odd
+if number % 2 == 0:
+    print("{} is an even number.".format(number))
+else:
+    print("{} is an odd number.".format(number))
