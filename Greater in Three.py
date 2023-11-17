b95ecb219dc439e4286523485ce4074238cdad16
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2023-11-17 12:13:05 +0600

    solve by python

---
+++ Greater in Three.py
@@ -0,0 +1,8 @@
+# Input three numbers
+num1, num2, num3 = map(int, input().split())
+
+# Find the maximum among three numbers
+maximum = max(num1, num2, num3)
+
+# Output the maximum
+print("{}".format(maximum))
