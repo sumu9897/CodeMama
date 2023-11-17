82c2a45c93bdf86e5b70f9834a88792eecea2e87
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2023-11-17 12:05:10 +0600

    solve by python

---
+++ Radius to Area.py
@@ -0,0 +1,10 @@
+import math
+
+# Input the radius of the circle
+radius = float(input())
+
+# Calculate the area of the circle
+area = 3.14 * (radius ** 2)
+
+# Output the area of the circle with two decimal points
+print("The area of the circle is {:.2f} square units.".format(area))
